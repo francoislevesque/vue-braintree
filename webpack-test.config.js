ddbfabbcea17980a08855450988d95f27f4fafb2
François Lévesque <francois.levesque@polymtl.ca>
2018-05-12 13:32:08 -0400

    Vue braintree coverage first draft

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -1,11 +1,24 @@
+var path = require('path');
 const { VueLoaderPlugin } = require('vue-loader')
+var isCoverage = process.env.NODE_ENV === 'coverage';
 
 module.exports = {
     mode: 'development',
+    output: {
+        // use absolute paths in sourcemaps (important for debugging via IDE)
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+    },
     module: {
         rules: [
             {
+                test: /.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            },
+            {
                 test: /\.vue$/,
+                exclude: /(node_modules|bower_components)/,
                 use: 'vue-loader'
             }
         ]
